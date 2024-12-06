6ac247f74d56407e1998b34587b0427b13cfcdc6
pranav3996 <yadavpranav456@gmail.com>
2024-12-06 14:48:44 +0530

    Booking Form

--- BHotel/src/main/java/com/bhotel/response/BookingResponse.java
+++ BHotel/src/main/java/com/bhotel/response/BookingResponse.java
@@ -31,7 +31,89 @@ public class BookingResponse {
 
     private RoomResponse room;
 
-    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate,
+    
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getCheckInDate() {
+		return checkInDate;
+	}
+
+	public void setCheckInDate(LocalDate checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+
+	public LocalDate getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public void setCheckOutDate(LocalDate checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
+
+	public String getGuestName() {
+		return guestName;
+	}
+
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+
+	public String getGuestEmail() {
+		return guestEmail;
+	}
+
+	public void setGuestEmail(String guestEmail) {
+		this.guestEmail = guestEmail;
+	}
+
+	public int getNumOfAdults() {
+		return numOfAdults;
+	}
+
+	public void setNumOfAdults(int numOfAdults) {
+		this.numOfAdults = numOfAdults;
+	}
+
+	public int getNumOfChildren() {
+		return numOfChildren;
+	}
+
+	public void setNumOfChildren(int numOfChildren) {
+		this.numOfChildren = numOfChildren;
+	}
+
+	public int getTotalNumOfGuests() {
+		return totalNumOfGuests;
+	}
+
+	public void setTotalNumOfGuests(int totalNumOfGuests) {
+		this.totalNumOfGuests = totalNumOfGuests;
+	}
+
+	public String getBookingConfirmationCode() {
+		return bookingConfirmationCode;
+	}
+
+	public void setBookingConfirmationCode(String bookingConfirmationCode) {
+		this.bookingConfirmationCode = bookingConfirmationCode;
+	}
+
+	public RoomResponse getRoom() {
+		return room;
+	}
+
+	public void setRoom(RoomResponse room) {
+		this.room = room;
+	}
+
+	public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate,
                            String bookingConfirmationCode) {
         this.id = id;
         this.checkInDate = checkInDate;
