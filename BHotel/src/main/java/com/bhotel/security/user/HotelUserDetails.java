f0ab32dde7ffc3f3c6d0fcf56c25129fffb808a9
pranav3996 <yadavpranav456@gmail.com>
2024-12-14 21:05:38 +0530

    Jwt Token

--- BHotel/src/main/java/com/bhotel/security/user/HotelUserDetails.java
+++ BHotel/src/main/java/com/bhotel/security/user/HotelUserDetails.java
@@ -1,5 +1,99 @@
 package com.bhotel.security.user;
 
-public class HotelUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bhotel.model.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HotelUserDetails implements UserDetails {
+	private Long id;
+	private String email;
+	private String password;
+	private Collection<GrantedAuthority> authorities;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAuthorities(Collection<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	public HotelUserDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HotelUserDetails(Long id, String email, String password, Collection<GrantedAuthority> authorities) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.authorities = authorities;
+	}
+
+	public static HotelUserDetails buildUserDetails(User user) {
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return new HotelUserDetails(user.getId(), user.getEmail(), user.getPassword(), authorities);
+
+	}
+
 
 }
--- BHotel/src/main/java/com/bhotel/security/user/HotelUserDetailsService.java
+++ BHotel/src/main/java/com/bhotel/security/user/HotelUserDetailsService.java
@@ -1,5 +1,27 @@
 package com.bhotel.security.user;
 
-public class HotelUserDetailsService {
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bhotel.model.User;
+import com.bhotel.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+    private  UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
 }
